715b70704a5147b79b37f3a1843d419be5b52432
NoJc <nojac42@gmail.com>
2024-05-01 20:44:34 +1000

    input basic structure of edit-task into create-task for referencing

--- src/components/create-task.js
+++ src/components/create-task.js
@@ -3,13 +3,40 @@ import {TaskModel} from '../models.js';
 
 
 class CreateTask extends LitElement {
+
+    static properties = {
+
+    }
+
+    static styles = css`
+
+    `;
+
+    connectedCallBack(){
+    super.connectedCallBack();
+
+    }
+
+    _submit(event){
+
+    }
+
+    render(){
+        return html`
+        `
+    }
+
     /* 
     Based on edit-task
     function will call a popup window similar to edit-task, with fields to define
     the content of the task with a cancel button and a create input element, which will
     an additional function updates the database to create a new task such that an extra
     task card will be rendered upon refresh.
-     */
+
+    EDIT: possibly make create-task its seperate button on the top of each of the task boards.
+    */
+
+
 }
 
 customElements.define('create-task', CreateTask);
